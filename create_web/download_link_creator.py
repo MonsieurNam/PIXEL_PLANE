1f9dbd07c403473bb2798c604e79a8f46d4f0140
MonsieurNam <126886123+MonsieurNam@users.noreply.github.com>
2024-06-06 01:47:05 +0700

    Update download_link_creator.py

--- create_web/download_link_creator.py
+++ create_web/download_link_creator.py
@@ -1,6 +1,7 @@
 import io
 import base64
 from PIL import Image
+import streamlit as st
 
 def get_image_download_link(image, filename, text):
     """Generates a link to download a particular image file."""
@@ -9,5 +10,5 @@ def get_image_download_link(image, filename, text):
     img.save(buffered, format='JPEG')
     img_str = base64.b64encode(buffered.getvalue()).decode()
     href = f'<a href="data:file/txt;base64,{img_str}" download="{filename}">{text}</a>'
-    #st.write(href)
-    return href
+    st.write(href)
+    # return href
